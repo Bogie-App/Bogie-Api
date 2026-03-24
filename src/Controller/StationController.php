3761a35ee24a0a3e65bdecbe58dfc79d8f13a942
hennotheo <theo.henno21@gmail.com>
2026-03-25 00:00:01 +0100

    :art: Add repo class

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -7,17 +7,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Nelmio\ApiDocBundle\Attribute\Model;
 use OpenApi\Attributes as OA;
-use App\Dto\StationBriefDto;
 use App\Dto\StationListResponseDto;
-use Doctrine\DBAL\Connection;
+use App\Repository\StationRepository;
 
 class StationController extends AbstractController
 {
-    private array $stations = [
-        ['Id' => 0, 'Name' => 'Name1'],
-        ['Id' => 1, 'Name' => 'Name2']
-    ];
-
     #[Route('api/stations', methods: ['GET'])]
     #[OA\Response(
         response: 200,
@@ -27,31 +21,9 @@ class StationController extends AbstractController
             items: new OA\Items(ref: new Model(type: StationListResponseDto::class, groups: ['full']))
         )
     )]
-    public function list(): JsonResponse
+    public function list(StationRepository $repo): JsonResponse
     {
-        $stations = array_map(
-            fn($s) => new StationBriefDto(
-                id: $s['Id'],
-                name: $s['Name']
-            ),
-            $this->stations
-        );
-
-        $response = new StationListResponseDto(
-            count: count($stations),
-            stations: $stations,
-            status: 'success'
-        );
-
-        return $this->json($response);
-    }
-
-    #[Route('api/test', methods: ['GET'])]
-    public function test(Connection $connection): JsonResponse
-    {
-        $stations = $connection->fetchAllAssociative(
-            'SELECT id, name FROM station'
-        );
+        $stations = $repo->findAllStations();
 
         return $this->json([
             'count' => count($stations),
